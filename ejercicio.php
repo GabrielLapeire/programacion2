3ac2e207e13ae893222b33a359ee1119e4e05a69
GabrielLapeire <81443834+GabrielLapeire@users.noreply.github.com>
2021-04-16 18:41:50 -0300

    Add files via upload

---
+++ ejercicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title> Programación II </title>
+	<link rel="stylesheet" type="text/css" href="estilos/style.css">
+</head>
+<body>
+	<h1> Palabra con acento </h1>
+		<p> brújula </p>
+	<h2> Palabra con acento </h2>
+		<p> brújula </p>
+	<h3> Palabra con acento </h3>
+		<p> brújula </p>
+</body>
+</html>
