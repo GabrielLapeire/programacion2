fb815e2b4e285612336b9a60a220e808bd368692
GabrielLapeire <81443834+GabrielLapeire@users.noreply.github.com>
2021-04-16 17:55:16 -0300

    Add files via upload

---
+++ select.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title> Programación II </title>
+	<style type="text/css"></style>
+</head>
+<body>
+	<select name="select">
+		<option>1</option>
+		<option>2</option>
+		<option>3</option>
+		<option>4</option>
+		<option>5</option>
+		<option>6</option>
+		<option>7</option>
+		<option>8</option>
+		<option>9</option>
+		<option>10</option>
+	</select>
+</body>
+</html>
